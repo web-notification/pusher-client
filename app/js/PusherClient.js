40d09b4e460d51db89953b87140b192c5b2b04e1
KimJiTae <jitae.kim@nhn.com>
2014-03-26 10:59:50 +0900

    * tray_icon 변경 * 서버 연결 끊어졌을때 다시 연결되도록 수정

--- app/js/PusherClient.js
+++ app/js/PusherClient.js
@@ -1,4 +1,7 @@
 var PusherClient = function(config) {
+    this._connected = false;
+    this.RETRY_INTERVAL = 1000;
+
     this.config = {
         push_server_url : ""
     }
@@ -10,7 +13,9 @@ var PusherClient = function(config) {
 PusherClient.prototype = {
     init: function(config) {
         $.extend(this.config, config);
-        this.socket = io.connect(this.config.push_server_url);
+
+        this._connectSocket();
+
         this.callbacks = {
             connected : function() {}
         }
@@ -19,11 +24,37 @@ PusherClient.prototype = {
     _attachEvent: function() {
         this.socket.on('registSuccess', $.proxy(this._onRegistSuccess, this));
         this.socket.on('receivePushMessage', $.proxy(this._onReceivePushMessage, this));
+        this.socket.on('connect', $.proxy(this._onConnectedSocket, this));
+        this.socket.on('disconnect', $.proxy(this._onDisconnectedSocket, this));
     },
-    connect: function(userid, password, callbacks) {
-        $.extend(this.callbacks, callbacks);
 
-        this.socket.emit('registNotification', { uniqueId: userid });
+    connect: function(userid, password, callbacks) {
+        this._uniqueId = userid;
+        $.extend(this.callbacks, callbacks);
+        this.socket.emit('registNotification', { uniqueId: this._uniqueId });
+    },
+
+    _connectSocket: function() {
+        this.socket = io.connect(this.config.push_server_url);
+    },
+
+    _onConnectedSocket: function() {
+        this._connected = true;
+    },
+
+    _reconnectSocket: function() {
+        if(this._connected === true) {
+            clearInterval(this._reconnectTimer);
+            return false;
+        }
+
+        this._connectSocket();
+        this.socket.emit('registNotification', { uniqueId: this._uniqueId });
+    },
+
+    _onDisconnectedSocket: function() {
+        this._connected = false;
+        this._reconnectTimer = setInterval($.proxy(this._reconnectSocket, this), this.RETRY_INTERVAL);
     },
 
     _onRegistSuccess: function (data) {
@@ -31,6 +62,12 @@ PusherClient.prototype = {
     },
 
     _onReceivePushMessage: function (data) {
+        var iconUrl = "";
+
+        if(data.icon) {
+            iconUrl = data.icon;
+        }
+
         window.LOCAL_NW.desktopNotifications.notify(data.icon, data.subject, data.description, function(){
             gui.Shell.openExternal(data.url)
         });
